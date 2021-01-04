9a1fca63fcdb5837a674bf9dbd85c06e325aa1ac
Fedenko Maksym <feden2906@gmail.com>
2021-01-04 10:27:30 +0200

    add multiply price for wishlist and cart

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,36 @@
 import React from "react";
 import './Header.css'
 import { useSelector } from "react-redux";
+import {useMemo} from "react";
 
 export const Header = () => {
-  const { cart, wishlist } = useSelector(
-    ({ cart: { cart }, wishlist: { wishlist } }) => ({
-      wishlist,
-      cart
-    })
-  );
+  const { cart, wishlist } = useSelector(({ cart: { cart }, wishlist: { wishlist } }) => ({wishlist, cart}));
+
+  const multiplyCart = useMemo(() => {
+    const cartClone = [...cart]
+    return cartClone.reduce((acc, item) => {
+      const {price} = item;
+      return acc + price
+    }, 0)
+  }, [cart])
+
+  const multiplyWishlist = useMemo(() => {
+    const wishlistClone = [...wishlist]
+    return wishlistClone.reduce((acc, item) => {
+      const {price} = item;
+      return acc + price
+    }, 0)
+  }, [wishlist])
 
   return (
     <header className="d-flex justify-between align-center">
       <h2>Hello in nedoshop</h2>
 
       <div className="d-flex justify-between">
-        <div className="mx-10 jn-tooltip" data-title={"cart"}>
+        <div className="mx-10 jn-tooltip" data-title={`Загальна сума : ${multiplyCart} $`}>
           cart: {cart.length}
         </div>
-        <div className="mx-10 jn-tooltip" data-title={"wishlist"}>
+        <div className="mx-10 jn-tooltip" data-title={`Загальна сума : ${multiplyWishlist} $`}>
           wishlist: {wishlist.length}
         </div>
       </div>
